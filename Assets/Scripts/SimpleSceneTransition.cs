ef55c6a3a1148c70362289dee2eecf43bea071b8
Robert Parker <rparker@kodama.design>
2018-09-16 12:59:45 -0700

    placeholder scenes

---
+++ Assets/Scripts/SimpleSceneTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleSceneTransition : MonoBehaviour
+{
+    [SerializeField]
+    private string scene;
+    [SerializeField]
+    private bool async = true;
+    [SerializeField]
+    private bool onAnyKey = true;
+
+    private void Update()
+    {
+        if(this.onAnyKey && Input.anyKeyDown)
+        {
+            this.Change();
+        }
+    }
+
+    public void Change()
+    {
+        if (this.async)
+        {
+            SceneManager.LoadSceneAsync(this.scene);
+        }
+        else
+        {
+            SceneManager.LoadScene(this.scene);
+        }
+    }
+}
