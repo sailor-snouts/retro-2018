23f34a65e5bd7f766884209b9963de389aaca8b6
Scott Anderson <scott@Conversas-MBP-2.localdomain>
2018-10-15 13:58:57 -0700

    more level 1 work but completely untested

--- Assets/Scripts/Levels/LightLiftController.cs
+++ Assets/Scripts/Levels/LightLiftController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: Currently only supports Vertical movement
 public class LightLiftController : MonoBehaviour {
 
     [SerializeField]
@@ -10,12 +11,41 @@ public class LightLiftController : MonoBehaviour {
     float speed = 4.0f;
     [SerializeField]
     bool up = true;
+    [SerializeField]
+    float maxDistance = 10.0f;
+    [SerializeField]
+    float maxIdleTime = 3.0f;
 
+    float startPos = 0.0f;
     bool moving = false;
     Transform playerTransform = null;
-    	
-	void FixedUpdate () {
+    float idleTime = 0.0f;
+
+    bool atMaxHeight = false;
+    bool resetPosition = false;
+
+    private void Start()
+    {
+        startPos = transform.position.x;
+    }
+
+    void FixedUpdate () {
         if(moving) {
+
+            float distance = Mathf.Abs(transform.position.x) - startPos;
+
+            if( distance >= maxDistance ) {
+                moving = false;
+                atMaxHeight = true;
+                return;
+            }
+
+            if (resetPosition && distance <= Mathf.Epsilon) {
+                resetPosition = false;
+                moving = false;
+                return;
+            }
+
             float moveDelta = Time.fixedDeltaTime * speed;
             moveDelta *= up ? 1 : -1;
 
@@ -23,7 +53,18 @@ public class LightLiftController : MonoBehaviour {
             Vector3 liftVector = new Vector3(0, moveDelta, 0);
             gameObject.transform.position += liftVector;
             playerTransform.position += liftVector;
+        } 
+
+        if(atMaxHeight) {
+            idleTime += Time.fixedDeltaTime;
+            if( idleTime >= maxIdleTime ) {
+                up = !up;
+                idleTime = 0.0f;
+                atMaxHeight = false;
+                resetPosition = true;
+            }
         }
+
 	}
 
     // TODO: Count # players for multiplayer support
@@ -32,8 +73,8 @@ public class LightLiftController : MonoBehaviour {
         if( collision.gameObject.tag == "Player" ){
             Debug.Log("Light Lift collision enter");
 
-            // TODO: This is hacky, but without it the player gets "stuck" in the rb2d
-            //collision.gameObject.transform.position += new Vector3(0, 0.13f, 0);
+            // Tracking current player transform to manage the collider positions
+            // while moving the platform during Update
             playerTransform = collision.gameObject.transform;
 
             moving = true;
