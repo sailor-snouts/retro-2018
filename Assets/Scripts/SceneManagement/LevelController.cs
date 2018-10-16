1548f97bcc0ac39e33f5ddf8218cc840190e99e1
Scott Anderson <scott@Conversas-MBP-2.localdomain>
2018-10-16 16:17:47 -0700

    first foray into camera rigging

--- Assets/Scripts/SceneManagement/LevelController.cs
+++ Assets/Scripts/SceneManagement/LevelController.cs
@@ -15,28 +15,41 @@ public class LevelController : MonoBehaviour {
         GameManager manager = FindObjectOfType<GameManager>();
         PlayerHealth playerHealth = FindObjectOfType<PlayerHealth>();
 
-        Transform playerTransform = null;
-
+        //Transform playerTransform = null;
+        int playerCount = 0;
         Debug.Log("LevelController::Start");
+
+        GameObject playerOne = null, playerTwo = null;
 
         if( manager ) {
             if( manager.playerOneActive ) {
-                GameObject playerOne = Instantiate(manager.GetPlayerOne());
+                playerOne = Instantiate(manager.GetPlayerOne());
                 playerOne.transform.position = playerOneSpawnPoint.transform.position;
                 playerOne.GetComponent<PlayerController>().SetPlayerNumber(1);
-                playerTransform = playerOne.transform;
+                //playerTransform = playerOne.transform;
+                playerCount++;
             }
 
             if (manager.playerTwoActive)
             {
-                GameObject playerTwo = Instantiate(manager.GetPlayerTwo());
+                playerTwo = Instantiate(manager.GetPlayerTwo());
                 playerTwo.GetComponent<PlayerController>().SetPlayerNumber(2);
                 playerTwo.transform.position = playerTwoSpawnPoint.transform.position;
+                playerCount++;
             }
         }
 
-        CinemachineVirtualCamera followCam = FindObjectOfType<CinemachineVirtualCamera>();
-        followCam.Follow = playerTransform;
+        CinemachineTargetGroup targetGroup = GetComponentInChildren<CinemachineTargetGroup>();
+        targetGroup.m_Targets = new CinemachineTargetGroup.Target[playerCount];
+        for (int i = 0; i < playerCount; i++ ) {
+            Cinemachine.CinemachineTargetGroup.Target target;
+            target.target = i == 0 ? playerOne.transform : playerTwo.transform;
+            target.weight = 1.0f;
+            target.radius = 0.0f;
+            targetGroup.m_Targets[i] = target;
+        }
+        //CinemachineVirtualCamera followCam = FindObjectOfType<CinemachineVirtualCamera>();
+        //followCam.Follow = playerTransform;
 
         PlayerInputManager inputManager = manager.GetComponent<PlayerInputManager>();
         inputManager.Restart();
