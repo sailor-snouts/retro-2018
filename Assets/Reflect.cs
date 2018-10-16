a9525a1d5e54f6590e34351d3e2f6026c9a84185
Robert Parker <rparker@kodama.design>
2018-10-15 19:06:29 -0700

    reflection script (not tested yet so box is disabled)

---
+++ Assets/Reflect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectProjectile : MonoBehaviour {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            collision.transform.localScale = new Vector3(-1 * collision.transform.localScale.x, collision.transform.localScale.y, collision.transform.localScale.z);
+        }
+    }
+}
