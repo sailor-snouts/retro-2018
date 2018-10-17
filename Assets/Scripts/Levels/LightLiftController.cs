b3eda954558fb91c8bd92e4a1a21d070e29f980b
Scott Anderson <scott@Conversas-MBP-2.localdomain>
2018-10-17 11:01:26 -0700

    lifts are now better because they are dumber

--- Assets/Scripts/Levels/LightLiftController.cs
+++ Assets/Scripts/Levels/LightLiftController.cs
@@ -19,27 +19,28 @@ public class LightLiftController : MonoBehaviour {
     float distanceTravelled = 0.0f;
     bool moving = false;
     Transform playerTransform = null;
+    bool on = false;
+
     //float idleTime = 0.0f;
 
     bool atMaxHeight = false;
     //bool resetPosition = false;
 
+    Vector3 startPosition, maxPosition;
+
     private void Start()
     {
+        startPosition = transform.position;
+        Vector3 maxVector = up ? new Vector3(0.0f, maxDistance) : new Vector3(0.0f, -maxDistance);
+        maxPosition = startPosition + maxVector;
     }
 
     void FixedUpdate () {
-        if(moving) {
 
-            if( distanceTravelled >= maxDistance ) {
-                moving = false;
-                atMaxHeight = true;
-                return;
-            }
+        if(moving) {
 
             float moveDelta = Time.fixedDeltaTime * speed;
             distanceTravelled += moveDelta;
-
             moveDelta *= up ? 1 : -1;
 
             // Clamped to PPU by speed setting
@@ -49,19 +50,24 @@ public class LightLiftController : MonoBehaviour {
             // If a player is on the lift, nudge them so their collider doesn't get stuck!
             if(playerTransform)
                 playerTransform.position += liftVector;
-        } 
 
-        if(atMaxHeight) {
-            atMaxHeight = false;
-            Debug.Log("Setting up ResetPosition call for " + maxIdleTime + "secs");
-            Invoke("ResetPosition", maxIdleTime);
+            atMaxHeight = up ? transform.position.y >= maxPosition.y : transform.position.y <= maxPosition.y;
+
+            if (atMaxHeight)
+            {
+                moving = false;
+                Debug.Log("Setting up ResetPosition call for " + maxIdleTime + "secs");
+                Invoke("ResetPosition", maxIdleTime);
+            }
         }
-
-	}
+    }
 
     private void ResetPosition() {
         Debug.Log("Resetting Lift Position");
         up = !up;
+        Vector3 maxVector = up ? new Vector3(0.0f, maxDistance) : new Vector3(0.0f, -maxDistance);
+        maxPosition = transform.position + maxVector;
+        Debug.Log("New maxPosition.y = " + maxPosition.y);
         moving = true;
         distanceTravelled = 0.0f;
     }
@@ -69,19 +75,22 @@ public class LightLiftController : MonoBehaviour {
     // TODO: Count # players for multiplayer support
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if( collision.gameObject.tag == "Player" ){
+        if (collision.gameObject.tag == "Player")
+        {
             playerTransform = collision.gameObject.transform;
-
-            moving = true;
+            if (!on && !moving) {
+                on = true;
+                moving = true;
+            }
         }
+
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (playerTransform)
         {
             playerTransform = null;
-            moving = false;
         }
     }
 }
